2ff2cfb4978e7a8363f23083124c11f70094e41a
xaviguasch <xaviguasch@gmail.com>
2019-02-05 16:34:21 +0100

    setting up routes

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.get('/', function (req, res) {
     res.send('<h1>Hello world</h1>')
 })
 
+app.get('/contact', function (req, res) {
+    res.send('<h2>Contact me at xaviguasch@gmail.com</h2>')
+})
+
+app.get('/about', function (req, res) {
+    res.send('<p>This is a quick bio of who I am for practice purposes, yeah</p>')
+})
+
+app.get('/hobbies', function (req, res) {
+    res.send('<p>A lot of hobbies</p>')
+})
+
 app.listen(3000, function () {
     console.log('server started on port 3000');
 })

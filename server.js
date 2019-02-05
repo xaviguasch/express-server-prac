06856690fe3bd052a3a8a64a74e8dbdf9c4a990e
xaviguasch <xaviguasch@gmail.com>
2019-02-05 16:13:35 +0100

    boilerplate starter files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello world</h1>')
+})
+
+app.listen(3000, function () {
+    console.log('server started on port 3000');
+})
